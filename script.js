7d9c58e464b27364963b978e9d3330d13e08db91
Ahmed Amine NAMMAT <ahmedaminenammat021105@gmail.com>
2025-03-31 02:20:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+'use srict' ;
+
+document.querySelector('.homeNav').addEventListener('mouseover', function() {
+    document.querySelector('.home').classList.remove('hidden') ;
+})
+document.querySelector('.homeNav').addEventListener('mouseout', function() {
+    document.querySelector('.home').classList.add('hidden') ;
+})
+
+
+document.querySelector('.cvNav').addEventListener('mouseover', function() {
+    document.querySelector('.cv').classList.remove('hidden') ;
+})
+document.querySelector('.cvNav').addEventListener('mouseout', function() {
+    document.querySelector('.cv').classList.add('hidden') ;
+})
+
+
+document.querySelector('.projectsNav').addEventListener('mouseover', function() {
+    document.querySelector('.projects').classList.remove('hidden') ;
+})
+document.querySelector('.projectsNav').addEventListener('mouseout', function() {
+    document.querySelector('.projects').classList.add('hidden') ;
+})
+
+
+document.querySelector('.project1').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover1').classList.remove('hidden') ;
+})
+document.querySelector('.project1').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover1').classList.add('hidden') ;
+})
+
+
+
+document.querySelector('.project2').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover2').classList.remove('hidden') ;
+})
+document.querySelector('.project2').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover2').classList.add('hidden') ;
+})
+
+
+
+document.querySelector('.project3').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover3').classList.remove('hidden') ;
+})
+document.querySelector('.project3').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover3').classList.add('hidden') ;
+})
+
+
+
+document.querySelector('.project4').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover4').classList.remove('hidden') ;
+})
+document.querySelector('.project4').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover4').classList.add('hidden') ;
+})
+
+
+
+document.querySelector('.project5').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover5').classList.remove('hidden') ;
+})
+document.querySelector('.project5').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover5').classList.add('hidden') ;
+})
+
+
+
+document.querySelector('.project6').addEventListener('mouseover', function() {
+    document.querySelector('.imageHover6').classList.remove('hidden') ;
+})
+document.querySelector('.project6').addEventListener('mouseout', function() {
+    document.querySelector('.imageHover6').classList.add('hidden') ;
+})
